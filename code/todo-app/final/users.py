36232d70c1112bec1747d4be6cfcac47359337d5
use-sparingly <alastair@alumedia.co.uk>
2018-11-24 17:55:24 +0000

    FEAT: Added Python3 support FIX: Py2 - Corrected assumed UTF-8 str encoding

--- code/todo-app/final/users.py
+++ code/todo-app/final/users.py
@@ -4,10 +4,13 @@ import getpass
 import argparse
 import hashlib
 import hmac
+import six
+import sys
 
 import boto3
 from boto3.dynamodb.types import Binary
 
+stdin_encoding = sys.stdin.encoding or 'utf-8'
 
 def get_table_name(stage):
     # We might want to user the chalice modules to
@@ -20,8 +23,19 @@ def get_table_name(stage):
 def create_user(stage):
     table_name = get_table_name(stage)
     table = boto3.resource('dynamodb').Table(table_name)
-    username = raw_input('Username: ').strip()
+    username = six.moves.input('Username: ').strip()
     password = getpass.getpass('Password: ').strip()
+
+    if isinstance(username, six.binary_type):
+        # Python >=2.x
+        # Convert to Unicode using console encoding
+        username = username.decode(stdin_encoding)
+
+    if isinstance(password, six.binary_type):
+        # Python >=2.x
+        # Convert to Unicode using console encoding
+        password = password.decode(stdin_encoding)
+
     password_fields = encode_password(password)
     item = {
         'username': username,
@@ -34,10 +48,17 @@ def create_user(stage):
 
 
 def encode_password(password, salt=None):
+    # Ensure password is Py2:Unicode,Py3:str
+    assert isinstance(password, six.text_type)
+
     if salt is None:
         salt = os.urandom(16)
+
+    # Encode password to UTF-8.
+    password_bytes = password.encode("UTF-8")
+
     rounds = 100000
-    hashed = hashlib.pbkdf2_hmac('sha256', password, salt, rounds)
+    hashed = hashlib.pbkdf2_hmac('sha256', password_bytes, salt, rounds)
     return {
         'hash': 'sha256',
         'salt': salt,
@@ -54,8 +75,19 @@ def list_users(stage):
 
 
 def test_password(stage):
-    username = raw_input('Username: ').strip()
+    username = six.moves.input('Username: ').strip()
     password = getpass.getpass('Password: ').strip()
+
+    if isinstance(username, six.binary_type):
+        # Python 2.x
+        # Convert to Unicode using console encoding
+        username = username.decode(stdin_encoding)
+
+    if isinstance(password, six.binary_type):
+        # Python 2.x
+        # Convert to Unicode using console encoding
+        password = password.decode(stdin_encoding)
+
     table_name = get_table_name(stage)
     table = boto3.resource('dynamodb').Table(table_name)
     item = table.get_item(Key={'username': username})['Item']
